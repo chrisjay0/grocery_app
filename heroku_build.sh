1f7ef7e310ac930e6defb712bac9bcd183100eac
Alttaab <alttaab@gmail.com>
2023-10-15 13:05:55 -0700

    Restructure for Heroku deployment

--- heroku_build.sh
+++ heroku_build.sh
@@ -2,6 +2,3 @@
 
 # Navigate to frontend and build the React app
 cd frontend && npm install && npm run build
-
-# Navigate to backend and install Python dependencies
-cd ../backend && pip install -r requirements.txt
