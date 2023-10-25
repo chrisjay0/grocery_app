f8053e4ccffc203e780bedd99f8314561ea1f5eb
Alttaab <alttaab@gmail.com>
2023-10-25 13:23:15 -0700

    Updating scripts for heroku.

--- backend/get_kr_locs.py
+++ backend/get_kr_locs.py
@@ -1,4 +1,9 @@
 from backend.services import parse_stores, fetch_locations
 from backend.util import update_stores
+import asyncio
 
-update_stores(parse_stores(fetch_locations()))
+
+
+unparsed_stores = asyncio.run(fetch_locations())
+
+update_stores(parse_stores(unparsed_stores))
